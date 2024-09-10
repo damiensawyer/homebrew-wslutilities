ce9e825fedf147f9afa5700a321bb49cb5fcf16d
Damien Sawyer <damiensawyer@gmail.com>
2024-09-11 07:54:25 +1000

    init

---
+++ Formula/wslu.rb
@@ -0,0 +1,29 @@
+class Wslu < Formula
+  desc "A collection of utilities for WSL"
+  homepage "https://github.com/wslutilities/wslu"
+  url "https://github.com/wslutilities/wslu/archive/v3.1.0.tar.gz"
+  sha256 "d5bfa71d3799d5fda50d5e13be87d77df71b5e1b61462f63d85d5ba54efc4f85"
+  version "3.1.0"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+
+  def install
+    # Run autoreconf to generate configure script
+    system "autoreconf", "-i"
+
+    # Ensure the configure script is executable
+    chmod "+x", "configure"
+
+    # Run configure and make
+    system "./configure", "--prefix=#{prefix}"
+    system "make"
+
+    # Install the binaries manually from the 'out' directory
+    bin.install Dir["out/*"]
+
+    # Optionally, install the man pages
+    man1.install Dir["out-docs/*.1.gz"]
+    man7.install Dir["out-docs/*.7.gz"]
+  end
+end
